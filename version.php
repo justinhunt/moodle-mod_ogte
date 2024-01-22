497aea4d95a477f6039dbd599e5d4882767903cd
justinhunt <bitwalkerjapan@gmail.com>
2024-01-22 06:17:58 +0000

    version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_ogte';
-$plugin->version  = 2024012200;
+$plugin->version  = 2024012201;
 $plugin->requires = 2017111300;  /* Moodle 3.4 */
 $plugin->release = '1.0.5 (Build: 2024012201)';
 $plugin->maturity = MATURITY_STABLE;
